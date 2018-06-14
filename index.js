b4523188dbbf08e52fc51017086d3ed7d27f9cc3
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2018-06-15 01:09:36 +0530

    ng build the Ang2

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ mongoose.connect(config.uri, (err) => {
   }
 });
 
-app.use(express.static(__dirname + `/client/dist/`));
+app.use(express.static(__dirname + `/client/dist/client/`));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + `/client/dist/index.html`));
+  res.sendFile(path.join(__dirname + `/client/dist/client/index.html`));
 });
 
 // hhtp://localhost:8000/
