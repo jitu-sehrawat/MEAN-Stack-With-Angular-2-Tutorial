35b69833c9c94472669c921756c6e25846c9ec37
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2018-06-15 01:00:24 +0530

    server-setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const config = require('./config/database')
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.uri, (err) => {
+  if(err) {
+    console.log(`Could not connect to database: ${err}`);
+  } else {
+    console.log(`Connected to database`);
+  }
+});
+
+app.use(express.static(__dirname + `/client/dist/`));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + `/client/dist/index.html`));
+});
+
+// hhtp://localhost:8000/
+app.listen(8000, () => {
+  console.log('Server started on http://localhost:8000/');
+})
